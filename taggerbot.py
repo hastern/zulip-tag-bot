f0ee4ffabb4bcd0f34d67de92058fd561d45a299
Hanno Sternberg <hanno@almostintelligent.de>
2020-09-30 23:33:50 +0200

    Move commands into separate handler functions and added proper i18n support

--- taggerbot.py
+++ taggerbot.py
@@ -1,19 +1,22 @@
 import collections
 import contextlib
+import dataclasses
 import difflib
-import logging
+import gettext
 import json
+import locale
+import logging
 import pathlib
 import textwrap
 
-from typing import Any, Dict, Optional, Tuple
+gettext.bindtextdomain("taggerbot", "locale")
+gettext.textdomain("taggerbot")
 
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+_ = gettext.gettext
 
 logger = logging.getLogger(__name__)
-
-
-class InvalidCommandError(Exception):
-    pass
 
 
 class MissingParameterError(Exception):
@@ -21,18 +24,17 @@ class MissingParameterError(Exception):
 
 
 class TagMapping:
-    STORAGE_KEY = "mapping"
-
-    def __init__(self):
+    def __init__(self, key="mapping"):
         self.tags = collections.defaultdict(set)
         self.users = collections.defaultdict(set)
         self.dirty = False
+        self.key = key
 
     def dump(self):
         return {t: list(us) for t, us in self.tags.items() if len(us) > 0}
 
     def load(self, storage):
-        d = storage.get(self.STORAGE_KEY, {})
+        d = storage.get(self.key, {})
         self.tags.clear()
         self.users.clear()
         self.dirty = False
@@ -44,7 +46,7 @@ class TagMapping:
 
     def store(self, storage):
         if self.dirty:
-            storage.put(self.STORAGE_KEY, self.dump())
+            storage.put(self.key, self.dump())
             self.dirty = False
         return self
 
@@ -69,9 +71,9 @@ class TagMapping:
     def find(self, tag=None, user=None):
         # assert tag is None ^ user is None
         if tag is not None:
-            return self.tags[tag.lower()]
+            return sorted(self.tags[tag.lower()])
         if user is not None:
-            return self.users[user]
+            return sorted(self.users[user])
         raise KeyError()
 
     def __contains__(self, tag):
@@ -140,9 +142,99 @@ def read_parameters(params):
     return set(map(str.strip, params[0].split(",")))
 
 
-CommandContext = collections.namedtuple(
-    "CommandContext", "handler parser", defaults=[lambda *e: None, lambda *e: []]
+CommandHandler = Callable[
+    [str, str, List[str], Dict[str, str], StorageContainer, Any], str
+]
+
+Command = collections.namedtuple(
+    "Command", "command syntax help handler", defaults=["", "", lambda *a: None]
 )
+
+
+def command_help(
+    sender: str,
+    command: str,
+    params: List[str],
+    storage: StorageContainer,
+    bot_handler: Any = None,
+) -> str:
+    return TaggerBotHandler.help_text()
+
+
+class Command_Manage:
+    def __init__(self, parser=lambda *a: [], mutator=lambda *a: None):
+        self.parser = parser
+        self.mutator = mutator
+
+    def __call__(
+        self,
+        sender: str,
+        command: str,
+        params: List[str],
+        storage: StorageContainer,
+        bot_handler: Any = None,
+    ) -> str:
+        all_tags = self.parser(params)
+        with TagMapping().use(storage) as tags:
+            self.mutator(tags, sender, *all_tags)
+            return _("Hi @**{}**, you are currently tagged with: {}").format(
+                sender, ", ".join(tags.find(user=sender)),
+            )
+
+
+def command_search(
+    sender: str,
+    command: str,
+    params: List[str],
+    storage: StorageContainer,
+    bot_handler: Any = None,
+) -> str:
+    all_tags = read_parameters(params)
+    with TagMapping().use(storage) as tags:
+        for tag in all_tags:
+            if tag.lower() not in tags:
+                nearest, ratio = tags.nearest(tag.lower())
+                if ratio > 0.75:
+                    return _("Hi @**{}**, I don't know the tag '{}'").format(
+                        sender, tag, nearest,
+                    )
+                else:
+                    return _(
+                        "Hi @**{}**, I don't know the tag '{}' - did you mean '{}'?"
+                    ).format(sender, tag,)
+
+        results = [tags.find(tag=tag) for tag in all_tags]
+        limit = set(storage.get("limit", []))
+        if len(limit) > 0:
+            intersection = limit.intersection(*results)
+        else:
+            intersection = results[0].intersection(*results[:1])
+        return _("Hi @**{}**, here's a list of everybody tagged with: {}\n\n{}").format(
+            sender,
+            (" " + _("and") + " ").join(all_tags),
+            "\n".join("- @**{}**".format(user) for user in intersection),
+        )
+
+
+def command_limit(
+    self,
+    sender: str,
+    command: str,
+    params: List[str],
+    storage: StorageContainer,
+    bot_handler: Any = None,
+) -> str:
+    if command == _("limit"):
+        users = read_parameters(params)
+        limit = set(storage.get("limit", []))
+        limit = limit.union(users)
+    elif command == _("unlimit"):
+        limit = []
+    storage.put("limit", list(limit))
+    if len(limit) > 0:
+        return _("Tag search is currently limited to: {}").format(", ".join(limit))
+    else:
+        return _("Tag search is currently unlimited")
 
 
 class TaggerBotHandler:
@@ -158,75 +250,93 @@ class TaggerBotHandler:
     - limit: A list of users that will be @mentioned.
     """
 
-    strings = {
-        "TAG_LIST": "Hi @**{}**, you are currently tagged with: {}",
-        "TAG_SEARCH": "Hi @**{}**, here's a list of everybody tagged with: {}\n\n{}",
-        "TAG_SEARCH_TYPO": "Hi @**{}**, I don't know the tag '{}' - did you mean '{}'?",
-        "TAG_SEARCH_UNKNOWN": "Hi @**{}**, I don't know the tag '{}'",
-        "TAG_JOIN_AND": "and",
-        "TAG_LIMIT": "Tag search is currently limited to: {}",
-        "TAG_UNLIMIT": "Tag search is currently unlimited",
-        "ERR_PARAM": "Sorry, I didn't understand you, a parameter is missing",
-        "ERR_COMMAND": "Sorry, '{}' is not a command I understand.",
-        "COMMAND_HELP": "help",
-        "COMMAND_LIST": "list",
-        "COMMAND_ADD": "add",
-        "COMMAND_REMOVE": "remove",
-        "COMMAND_SEARCH": "search",
-        "COMMAND_LIMIT": "limit",
-        "COMMAND_UNLIMIT": "unlimit",
-        "HELP_TEXT": """This plugin allows users to store and query the tag-set of other users.""",
-        "HELP_HELP": "To show all commands the bot supports.",
-        "HELP_LIST": "Show all tags currently applied to the user",
-        "HELP_ADD": "To add personal tag(s).",
-        "HELP_REMOVE": "To remove personal tag(s).",
-        "HELP_SEARCH": "To search for somebody with <tag>.",
-        "HELP_LIMIT": "Limit search to this group of users.",
-        "HELP_UNLIMIT": "Remove all search limits.",
-        "SYNTAX_HELP": "",
-        "SYNTAX_LIST": "",
-        "SYNTAX_ADD": "<tag>, <tag> ...",
-        "SYNTAX_REMOVE": "<tag>, <tag>, ...",
-        "SYNTAX_SEARCH": "<tag>, <tag>, ...",
-        "SYNTAX_LIMIT": "<user>, <user>, ...",
-        "SYNTAX_UNLIMIT": "",
-    }
-
     META = {
         "name": "Tagger",
         "description": "Allows user tag store/lookup",
     }
 
+    strings: Dict[str, str] = {}  # For i18n of all text
+
+    @classmethod
+    def build_commands(cls) -> List[Tuple[str, str, str, Command]]:
+        """
+        Build a dictionary of commands.
+
+        Note: This has to be done after the configuration with i18n
+        options has been loaded, otherwise the strings dictionary would
+        be available.
+        """
+        cls.commands = [
+            Command(
+                _("help"),
+                "",
+                _("To show all commands the bot supports."),
+                command_help,
+            ),
+            Command(
+                _("list"),
+                "",
+                _("Show all tags currently applied to the user."),
+                Command_Manage(),
+            ),
+            Command(
+                _("add"),
+                _("<tag>, <tag>, ..."),
+                _("To add personal tag(s)."),
+                Command_Manage(read_parameters, TagMapping.add),
+            ),
+            Command(
+                _("remove"),
+                _("<tag>, <tag>, ..."),
+                _("To remove personal tag(s)."),
+                Command_Manage(read_parameters, TagMapping.remove),
+            ),
+            Command(
+                _("search"),
+                _("<tag>, <tag>, ..."),
+                _("To search for somebody with <tag>."),
+                command_search,
+            ),
+            Command(
+                _("limit"),
+                _("<user>, <user>, ..."),
+                _("Limit search to this group of users."),
+                command_limit,
+            ),
+            Command(_("unlimit"), "", _("Remove all search limits."), command_limit),
+        ]
+        return {cmd.command: cmd.handler for cmd in cls.commands}
+
+    @classmethod
+    def help_text(cls, bot_name="mention-bot"):
+        return "{}\n\n{}".format(
+            _(
+                "This plugin allows users to store and query the tag-set of other users."
+            ),
+            "\n".join(
+                f"- @{bot_name} {cmd.command}: {cmd.syntax}  -> {cmd.help}"
+                for cmd in cls.commands
+            ),
+        )
+
     def initialize(self, bot_handler: Any) -> None:
         self.storage = ""
         self.config_info = bot_handler.get_config_info("TaggerBot")
         for key, val in self.config_info.items():
-            if key.startswith("string_"):
-                self.strings[key[7:].upper()] = val
+            if key == "language":
+                lang = gettext.translation(
+                    domain="taggerbot", localedir="locale", languages=[val]
+                )
+                lang.install()
+                global _
+                _ = lang.gettext
             elif key == "storage" and val.endswith(".json"):
                 self.storage = val
         logger.debug(self.config_info)
+        self.commands = self.build_commands()
 
     def usage(self) -> str:
-        return "{}\n\n{}".format(
-            self.strings["HELP_TEXT"],
-            "\n".join(
-                "- @mention-bot {}: {}  -> {}".format(
-                    self.strings["COMMAND_{}".format(cmd)],
-                    self.strings["SYNTAX_{}".format(cmd)],
-                    self.strings["HELP_{}".format(cmd)],
-                )
-                for cmd in [
-                    "HELP",
-                    "LIST",
-                    "ADD",
-                    "REMOVE",
-                    "SEARCH",
-                    "LIMIT",
-                    "UNLIMIT",
-                ]
-            ),
-        )
+        return self.help_text()
 
     def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
         quoted_name = bot_handler.identity().mention
@@ -246,92 +356,18 @@ class TaggerBotHandler:
         else:
             sender = message["sender_email"]
         try:
-            if command in [self.strings["COMMAND_HELP"]]:
-                bot_handler.send_reply(message, self.usage())
-            elif command in [
-                self.strings["COMMAND_LIST"],
-                self.strings["COMMAND_ADD"],
-                self.strings["COMMAND_REMOVE"],
-            ]:
-                cmd_funcs = {
-                    self.strings["COMMAND_ADD"]: CommandContext(
-                        TagMapping.add, read_parameters
-                    ),
-                    self.strings["COMMAND_REMOVE"]: CommandContext(
-                        TagMapping.remove, read_parameters
-                    ),
-                }
-                ctx = cmd_funcs.get(command, CommandContext())
-                all_tags = ctx.parser(params)
-                with TagMapping().use(storage) as tags:
-                    ctx.handler(tags, sender, *all_tags)
-                    bot_handler.send_reply(
-                        message,
-                        self.strings["TAG_LIST"].format(
-                            sender, ", ".join(tags.find(user=sender)),
-                        ),
-                    )
-            elif command in [self.strings["COMMAND_SEARCH"]]:
-                all_tags = read_parameters(params)
-                with TagMapping().use(storage) as tags:
-                    for tag in all_tags:
-                        if tag.lower() not in tags:
-                            nearest, ratio = tags.nearest(tag.lower())
-                            if ratio > 0.75:
-                                bot_handler.send_reply(
-                                    message,
-                                    self.strings["TAG_SEARCH_TYPO"].format(
-                                        sender, tag, nearest,
-                                    ),
-                                )
-                            else:
-                                bot_handler.send_reply(
-                                    message,
-                                    self.strings["TAG_SEARCH_UNKNOWN"].format(
-                                        sender, tag,
-                                    ),
-                                )
-                            return
-                    results = [tags.find(tag=tag) for tag in all_tags]
-                    limit = set(storage.get("limit", []))
-                    if len(limit) > 0:
-                        intersection = limit.intersection(*results)
-                    else:
-                        intersection = results[0].intersection(*results[:1])
-                    bot_handler.send_reply(
-                        message,
-                        self.strings["TAG_SEARCH"].format(
-                            sender,
-                            (" " + self.strings["TAG_JOIN_AND"] + " ").join(all_tags),
-                            "\n".join(
-                                "- @**{}**".format(user) for user in intersection
-                            ),
-                        ),
-                    )
-            elif command in [self.strings["COMMAND_LIMIT"]]:
-                users = read_parameters(params)
-                limit = set(storage.get("limit", []))
-                limit = limit.union(users)
-                if len(limit) > 0:
-                    bot_handler.send_reply(
-                        message, self.strings["TAG_LIMIT"].format(", ".join(limit)),
-                    )
-                else:
-                    bot_handler.send_reply(
-                        message, self.strings["TAG_UNLIMIT"],
-                    )
-                storage.put("limit", list(limit))
-            elif command in [self.strings["COMMAND_UNLIMIT"]]:
-                storage.put("limit", [])
-                bot_handler.send_reply(
-                    message, self.strings["TAG_UNLIMIT"],
-                )
-            else:
-                raise InvalidCommandError(command)
+            handler = self.commands[command]
+            response = handler(sender, command, params, storage, bot_handler)
+            if response is not None:
+                bot_handler.send_reply(message, response)
         except MissingParameterError:
-            bot_handler.send_reply(message, self.strings["ERR_PARAM"])
-        except InvalidCommandError as err:
-            bot_handler.send_reply(message, self.strings["ERR_COMMAND"].format(err))
+            bot_handler.send_reply(
+                message, _("Sorry, I didn't understand you, a parameter is missing")
+            )
+        except KeyError as err:
+            bot_handler.send_reply(
+                message, _("Sorry, '{}' is not a command I understand.").format(err)
+            )
         except Exception as err:
             logger.exception(err)
 
